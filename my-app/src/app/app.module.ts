92bf3121bf770ac3ff6cf0ea532b504d4df72a6f
daliya john <daliyajohn75@gmail.com>
2017-09-27 09:54:59 +0400

    import underscore.js

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdCardModule } from '@angular/material';
+import * as _ from 'underscore';
 
 import { AppComponent } from './app.component';
 
